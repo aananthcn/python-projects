d9f30dbc78c6ce570efe019c70874c02cc6bcd47
Aananth C N <aananth.cn@visteon.com>
2018-09-24 10:00:59 +0530

    Code clean up - MathPlot

--- Basic/MathPlot.py
+++ Basic/MathPlot.py
@@ -8,35 +8,36 @@ plt.figure(1)
 # First order polynomial
 y = 1 + x
 plt.subplot(331)
-plt.plot(x,y, 'bo')
+# plt.plot(x,y, 'bo')
+plt.plot(x, y)
 plt.title("1st Order")
 
 
 # Second order polynomial
 y = 1 + x + x*x
 plt.subplot(332)
-plt.plot(x, y, '*')
+plt.plot(x, y)
 plt.title("2nd Order")
 
 
 # 3rd order polynomial
 y = 1 + x + x*x + x*x*x
 plt.subplot(333)
-plt.plot(x,y, '+')
+plt.plot(x, y)
 plt.title("3rd Order")
 
 
 # 4th order polynomial
 y = 1 + x + x*x + x*x*x + x*x*x*x
 plt.subplot(334)
-plt.plot(x,y)
+plt.plot(x, y)
 plt.title("4th Order")
 
 
 # exponential
 y = np.exp(x)
 plt.subplot(335)
-plt.plot(x,y)
+plt.plot(x, y)
 plt.title("Exponential")
 
 
@@ -44,17 +45,15 @@ plt.title("Exponential")
 t = np.arange(0, 720, 5)
 y = np.sin(np.deg2rad(x))
 plt.subplot(336)
-plt.plot(x,y)
+plt.plot(x, y)
 plt.title("Sine")
 
 
 # Cosine
 y = np.cos(np.deg2rad(x))
 plt.subplot(337)
-plt.plot(x,y)
+plt.plot(x, y)
 plt.title("Cosine")
-
-
 
 
 plt.show()
