3041ecb9bbc3bd9261bc45e510ba41771f4bc9f5
Aananth C N <aananth.cn@visteon.com>
2018-11-20 19:17:25 +0530

    Adding excercises using OpenCV Object Tracking by Colors, Background Subtraction, Object Tracking using CamShift Algorithm.

--- ML/California-Housing/CaliforniaHousing.py
+++ ML/California-Housing/CaliforniaHousing.py
@@ -67,8 +67,60 @@ housing.plot(kind="scatter", x="longitude", y="latitude", alpha=0.4,
              s=housing["population"]/100, label="population", figsize=(10, 7),
              c="median_house_value", cmap=plt.get_cmap("jet"), colorbar=True)
 plt.legend()
-plt.show()
+#plt.show()
 
+
+
+## Experimenting with Attribute Combinations
 corr_matrix = housing.corr()
 corr_matrix["median_house_value"].sort_values(ascending=False)
 print(corr_matrix)
+
+
+
+## prepare the data for ML algorithm
+housing = strat_train_set.drop("median_house_value", axis=1)
+housing_labels = strat_train_set["median_house_value"].copy()
+
+
+
+## Data Cleaning
+from sklearn.preprocessing import Imputer
+
+# housing.dropna(subset=["total_bedrooms"])     # option 1
+# housing.drop("total_bedrooms", axis=1)        # option 2
+median = housing["total_bedrooms"].median()     # option 3
+housing["total_bedrooms"].fillna(median, inplace=True)
+
+imputer = Imputer(strategy="median")
+housing_num = housing.drop("ocean_proximity", axis=1) # dropping because of text attribute
+imputer.fit(housing_num)
+
+X=imputer.transform(housing_num)
+housing_tr = pd.DataFrame(X, columns=housing_num.columns)
+
+
+
+## Custom Transformers
+from sklearn.base import BaseEstimator, TransformerMixin
+
+rooms_ix, bedrooms_ix, population_ix, household_ix = 3, 4, 5, 6
+
+class CombinedAttributesAdder(BaseEstimator, TransformerMixin):
+    def __init__(self, add_bedrooms_per_room = True): # no *args or **kargs
+        self.add_bedrooms_per_room = add_bedrooms_per_room
+    def fit(self, X, y=None):
+        return self # nothing else to do
+    def transform(self, X, y=None):
+        rooms_per_household = X[:, rooms_ix] / X[:, household_ix]
+        population_per_household = X[:, population_ix] / X[:, household_ix]
+        if self.add_bedrooms_per_room:
+            bedrooms_per_room = X[:, bedrooms_ix] / X[:, rooms_ix]
+            return np.c_[X, rooms_per_household, population_per_household, bedrooms_per_room]
+        else:
+            return np.c_[X, rooms_per_household, population_per_household]
+
+
+attr_adder = CombinedAttributesAdder(add_bedrooms_per_room=False)
+housing_extra_attribs = attr_adder.transform(housing.values)
+
